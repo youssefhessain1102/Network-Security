e397bd50ec4ec2bf4a8dae065012004f343cb063
youssefhessain1102 <youssefhussainnasr2000@gmail.com>
2025-11-29 20:15:24 +0200

    mongodb_changes

--- push_data.py
+++ push_data.py
@@ -3,8 +3,43 @@ import sys
 import json
 import certifi
 from dotenv import load_dotenv
+import pandas as pd
+import pymongo
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
 
 load_dotenv()
 
 MONGO_DB_URL=os.getenv("MONGO_DB_URL")
 print(MONGO_DB_URL)
+
+ca = certifi.where()
+
+class NetworkDataExtraction():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+        def csv_to_json(self, file_path):
+            try:
+                data = pd.read_csv(file_path)
+                data.reset_index(drop=True, inplace=True)
+
+                records = list(json.loads(data.T.to_json()).values())
+
+                return records
+            except Exception as e:
+                raise NetworkSecurityException(e, sys)
+        
+        def insert_data_to_mongodb(self, records, datbase, collection):
+            try:
+                self.records = records
+                self.database = datbase
+                self.collection = collection
+
+                self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+
+            except Exception as e:
+                raise NetworkSecurityException(e, sys)
--- test_mongodb.py
+++ test_mongodb.py
@@ -1,10 +1,12 @@
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://youssefhessain1102:youssef1102@cluster0.xhyyvtr.mongodb.net/?appName=Cluster0"
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(MONGO_DB_URL, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
